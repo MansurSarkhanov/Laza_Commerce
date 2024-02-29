7c5a15706191968a25a5bfd889c1dffc9cfd207e
MansurSarkhanov <sarxanov.mansur@gmail.com>
2024-02-29 12:34:07 +0400

    test

--- laza_commerce/lib/Core/Bloc/Home/home_state.dart
+++ laza_commerce/lib/Core/Bloc/Home/home_state.dart
@@ -10,29 +10,19 @@ sealed class HomeState extends Equatable {
 final class HomeInitial extends HomeState {}
 
 final class HomeSuccess extends HomeState {
-  final List<ProductModel>? productList;
-  final List<CategoryModel>? categoryList;
+  final List<ProductModel> productList;
+  final List<CategoryModel> categoryList;
   final String selectedCategory;
-  final UserModel? user;
-  final bool isLoading;
-
+  final UserModel user;
 
   const HomeSuccess({
     this.selectedCategory = '',
-    this.categoryList,
-    this.productList,
-    this.user,
-    this.isLoading = false,
+    required this.categoryList,
+    required this.productList,
+    required this.user,
   });
-  HomeSuccess copyWith({categoryList, productList, user, selectedCategory, isLoading}) => HomeSuccess(
-      isLoading: isLoading ?? this.isLoading,
-      categoryList: categoryList ?? this.categoryList,
-      productList: productList ?? this.productList,
-      user: user ?? this.user,
-      selectedCategory: selectedCategory ?? this.selectedCategory);   
 }
 
 final class HomeProgress extends HomeState {}
-
 
 final class HomeFailure extends HomeState {}
