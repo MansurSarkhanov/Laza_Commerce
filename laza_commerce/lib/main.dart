63d638a52388868c974edd33006a2b511f66fe1e
MansurSarkhanov <sarxanov.mansur@gmail.com>
2024-03-04 20:24:33 +0400

    test

--- laza_commerce/lib/main.dart
+++ laza_commerce/lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
             create: (context) => HomeCubit(),
             child: MaterialApp(
               debugShowCheckedModeBanner: false,
-              title: 'Laza Commerce',
+              title: 'Laza Commerce App',
               theme: ThemeData(
                 colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
                 useMaterial3: true,
